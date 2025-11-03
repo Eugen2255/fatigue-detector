a9b8c84e92a6e56cfe167cac5d01e4952976829c
Eugen <157390965+Eugen2255@users.noreply.github.com>
2025-11-03 23:45:14 +0700

    Update __init__.py

--- src/detectors/__init__.py
+++ src/detectors/__init__.py
@@ -2,7 +2,7 @@ from .pose_detector import PoseDetector
 from .face_detector import FaceDetector
 
 
-__verion__ = '0.0.1'
+__version__ = '0.0.1'
 
 # from detectors import *
 __all__ = ['PoseDetector', 'FaceDetector']
