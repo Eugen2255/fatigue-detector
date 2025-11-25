56c7debc5e3f5489ce35b91701de6da5c812f1e1
Eugen <157390965+Eugen2255@users.noreply.github.com>
2025-11-25 17:40:30 +0700

    fixed version

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,7 +1,7 @@
 from .eyes_rub import detect_rubbing
 from .head_tilt import detect_head_tilt
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 # from detectors import *
 __all__ = ['detect_rubbing', 'detect_head_tilt']
